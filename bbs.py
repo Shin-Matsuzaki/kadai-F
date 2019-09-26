ae68c5d8d8d5f67d9e356c59ab4af4c9ac641b8b
matsuzaki <shin.matsu.p@gmail.com>
2019-09-26 12:47:59 +0900

    掲示板アプリのプロトタイプを実装

--- bbs.py
+++ bbs.py
@@ -5,11 +5,43 @@
 ・水平線区切りで投稿が表示　Username未記入で「名無しさん」になる
 ・投稿された内容が保存される
 """
-
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-if __name__ == '__main__':
+@app.route('/bbs', methods=["GET", "POST"])
+def index():
+    post_list = []
+    if request.method == 'GET':
+        return render_template('bbs.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        message = request.form['message']
+        if username == '':
+            username = '名無しさん'
+
+        with open('post.csv', 'a') as f:
+            f.write(f'{username}, {message}\n')
+        with open('post.csv', 'r') as f:
+            line = f.readline()[:-1]
+            while line:
+                row = line.split(',')
+                post_list.append(row)
+                line = f.readline()[:-1]
+        return render_template('bbs.html', post_data=post_list)
+
+
+def main():
+    # 1行目にタグ付けして管理しようかと頑張った痕跡
+    # if not os.path.isfile("post.csv"):
+    #     with open("post.csv", "w") as f:
+    #         writer = csv.writer(f)
+    #         writer.writerow(['username', 'message'])
+
     app.run(debug=True, port=8888)
+
+
+if __name__ == '__main__':
+    main()
